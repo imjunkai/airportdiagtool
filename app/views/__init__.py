87f5561be3cf05e267e84188f9e393f5cad36771
imjunkai <junkai_wong@mymail.sutd.edu.sg>
2017-06-07 14:30:28 +0800

    deleting everything

--- app/views/__init__.py
+++
@@ -1,6 +0,0 @@
-# from app import app
-# from flask import render_template, flash, redirect, url_for
-
-# @app.route('/')
-# def hello():
-# 	return render_template('main/landingpage.html')
--- config.py
+++
@@ -1,4 +0,0 @@
-WTF_CSRF_ENABLED = True
-SECRET_KEY = 'you-will-never-guess'
-
-SQLALCHEMY_DATABASE_URI = "mysql://root:password@localhost/new_adt"
