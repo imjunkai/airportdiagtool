a2cfcfa29a4923c0704b7365addd172b575ccd9c
imjunkai <junkai_wong@mymail.sutd.edu.sg>
2017-06-14 11:26:53 +0800

    Updated database with data attained from data analysis

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
 SQLALCHEMY_DATABASE_URI = "mysql://root:password@localhost/new_adt"
-# SQLALCHEMY_DATABASE_URI = "mysql://cai:caicapstone@128.199.161.173/new_adt"
+# SQLALCHEMY_DATABASE_URI = "mysql://root:caicapstone@localhost/new_adt"
